7658c32f9d159f2e11e3be4c769b79ff1c710e3b
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-02-06 21:20:15 +0800

    install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+  FieldTitle,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <FieldSet>
+        <FieldLegend>Profile</FieldLegend>
+        <FieldDescription>
+          This appears on invoices and emails.
+        </FieldDescription>
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="name">Full name</FieldLabel>
+            <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
+            <FieldDescription>
+              This appears on invoices and emails.
+            </FieldDescription>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="username">Username</FieldLabel>
+            <Input id="username" autoComplete="off" aria-invalid />
+            <FieldError>Choose another username.</FieldError>
+          </Field>
+          <Field orientation="horizontal">
+            <Switch id="newsletter" />
+            <FieldLabel htmlFor="newsletter">
+              Subscribe to the newsletter
+            </FieldLabel>
+          </Field>
+        </FieldGroup>
+      </FieldSet>
+    </>
+  );
 }
 
 export default App;
