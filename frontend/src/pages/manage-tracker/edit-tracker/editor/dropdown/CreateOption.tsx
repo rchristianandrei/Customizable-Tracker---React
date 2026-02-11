f7270fd8999476fb9460a944b0262155105b11cd
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-02-11 15:53:21 +0800

    create option form

--- frontend/src/pages/manage-tracker/edit-tracker/editor/dropdown/CreateOption.tsx
+++ frontend/src/pages/manage-tracker/edit-tracker/editor/dropdown/CreateOption.tsx
@@ -6,11 +6,44 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Field } from "@/components/ui/field";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  label: z
+    .string()
+    .min(1, "Label must be at least 1 character.")
+    .max(32, "Label must be at most 32 characters."),
+  value: z
+    .string()
+    .min(1, "Value must be at least 1 character.")
+    .max(32, "Value must be at most 32 characters."),
+});
 
 export function CreateOption() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      label: "",
+      value: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    form.reset();
+    console.log(data);
+    console.log("submit");
+  }
+
   return (
     <Dialog>
       <DialogTrigger>Add</DialogTrigger>
@@ -22,25 +55,49 @@ export function CreateOption() {
         <DialogHeader>
           <DialogTitle>Create Option</DialogTitle>
         </DialogHeader>
-        <Field className="gap-1">
-          <Label>Label</Label>
-          <Input
-            name="label"
-            //   onChange={handleChange}
-            type="text"
-            // value={selectedComponent.name}
-          ></Input>
-        </Field>
-        <Field className="gap-1">
-          <Label>Value</Label>
-          <Input
-            name="value"
-            //   onChange={handleChange}
-            type="text"
-            // value={selectedComponent.name}
-          ></Input>
-        </Field>
-        <Button variant="default">Add</Button>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-2">
+            <Controller
+              name="label"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">Label</FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-title"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Ph"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="value"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">Value</FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-title"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Philippines"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </FieldGroup>
+        </form>
       </DialogContent>
     </Dialog>
   );
